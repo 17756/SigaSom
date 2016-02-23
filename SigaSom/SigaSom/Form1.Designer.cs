73603dbcf6faf158a0207afe224525a0efcb014b
17756 <17756@aeffl.pt>
2016-02-23 15:45:28 +0000

    commit

--- SigaSom/SigaSom/Form1.Designer.cs
+++ SigaSom/SigaSom/Form1.Designer.cs
@@ -29,32 +29,108 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(256, 155);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(124, 92);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 92);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(230, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 92);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 202);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 92);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button_Start
+            // 
+            this.button_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button_Start.Location = new System.Drawing.Point(230, 138);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(104, 31);
+            this.button_Start.TabIndex = 4;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(20, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Vez do Jogador1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(358, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Vez do Jogador2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 420);
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.ClientSize = new System.Drawing.Size(369, 310);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Crimson;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- SigaSom/SigaSom/Form1.cs
+++ SigaSom/SigaSom/Form1.cs
@@ -16,5 +16,42 @@ namespace SigaSom
         {
             InitializeComponent();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            Random rnd = new Random();
+            int but = rnd.Next(1, 5);
+            switch (but)
+            {
+                case 1:
+                    button1.BackColor = Color.DeepPink;
+                    System.Media.SystemSounds.Beep.Play();
+                    break;
+                case 2:
+                    button2.BackColor = Color.DeepPink;
+                    System.Media.SystemSounds.Asterisk.Play();
+                    break;
+                case 3:
+                    button3.BackColor = Color.DeepPink;
+                    System.Media.SystemSounds.Exclamation.Play();
+                    break;
+                case 4:
+                    button4.BackColor = Color.DeepPink;
+                    System.Media.SystemSounds.Hand.Play();
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
     }
 }
